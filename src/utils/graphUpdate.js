d6477606cd5f7f25d9f63f2ca8849eb9bf34b1a4
SonghaiFan <songhai.fan2022@gmail.com>
2024-01-08 06:48:12 +1100

    improve graph update animation

--- src/utils/graphUpdate.js
+++ src/utils/graphUpdate.js
@@ -1,9 +1,10 @@
-import { layoutConfig } from "@/config/graphLayout";
-
 export function updateGraph(cy, newData, layout) {
   const currentNodes = cy.nodes();
   const currentEdges = cy.edges();
   const { nodes: newNodes, edges: newEdges } = newData;
+
+  let removedElements = [];
+  let layoutUpdated = false; // Flag to prevent repeated animations
 
   const processElements = (currentElements, newElements) => {
     const currentIds = currentElements.map((ele) => ele.id());
@@ -26,11 +27,40 @@ export function updateGraph(cy, newData, layout) {
     const toRemove = currentElements.filter(
       (ele) => !newIds.includes(ele.id())
     );
-    cy.remove(toRemove);
+
+    removedElements = removedElements.concat(toRemove);
   };
 
   processElements(currentNodes, newNodes);
   processElements(currentEdges, newEdges);
 
   cy.layout(layout).run();
+
+  cy.on("layoutstop", () => {
+    if (layoutUpdated) return; // Check if the layout was already updated
+    layoutUpdated = true; // Set the flag
+
+    let removePromises = removedElements.map((ele) => {
+      return new Promise((resolve) => {
+        ele.animate({
+          style: { opacity: 0 },
+          duration: 200,
+          complete: () => {
+            ele.remove();
+            resolve();
+          },
+        });
+      });
+    });
+
+    Promise.all(removePromises).then(() => {
+      cy.animate({
+        fit: {
+          eles: cy.elements(), // fit to all remaining elements
+          padding: 10, // padding around the elements
+        },
+        duration: 500, // duration of the animation
+      });
+    });
+  });
 }
